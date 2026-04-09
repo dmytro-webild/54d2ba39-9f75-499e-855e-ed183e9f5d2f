d0facef15468f192725f98d4ee0873411335cd28
bender <kudin.dima@gmail.com>
2026-04-09 20:56:29 +0000

    Update src/app/how-it-works/page.tsx

--- src/app/how-it-works/page.tsx
+++ src/app/how-it-works/page.tsx
@@ -29,9 +29,10 @@ export default function HowItWorksPage() {
               { name: "Services", id: "/services" },
               { name: "How It Works", id: "/how-it-works" },
               { name: "FAQ", id: "/faq" },
+              { name: "Contact", id: "/contact" },
             ]}
             brandName="South Bay Mini Splits"
-            button={{ text: "Get Quote", href: "#" }}
+            button={{ text: "Get Quote", href: "/contact" }}
           />
         </div>
 
