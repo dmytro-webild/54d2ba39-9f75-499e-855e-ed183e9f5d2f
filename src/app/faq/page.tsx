c1828373308f4470e0b07996509f89fa8e1a4f8b
bender <kudin.dima@gmail.com>
2026-04-30 19:11:49 +0000

    Update src/app/faq/page.tsx

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -26,7 +26,6 @@ export default function FaqPage() {
             { name: "Home", id: "/" },
             { name: "About", id: "/about" },
             { name: "Services", id: "/services" },
-            { name: "How It Works", id: "/how-it-works" },
             { name: "FAQ", id: "/faq" },
             { name: "Testimonials", id: "/testimonials" },
             { name: "Contact", id: "/contact" },
