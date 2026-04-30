5aa670743a50b3d6796b5b8298e1a6db71da2a7e
bender <kudin.dima@gmail.com>
2026-04-30 19:10:43 +0000

    Update src/app/how-it-works/page.tsx

--- src/app/how-it-works/page.tsx
+++ src/app/how-it-works/page.tsx
@@ -29,6 +29,7 @@ export default function HowItWorksPage() {
               { name: "Services", id: "/services" },
               { name: "How It Works", id: "/how-it-works" },
               { name: "FAQ", id: "/faq" },
+              { name: "Testimonials", id: "/testimonials" },
               { name: "Contact", id: "/contact" },
             ]}
             brandName="South Bay Mini Splits"
